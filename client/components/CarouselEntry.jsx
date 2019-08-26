c0dddbd4a399f627f7c54d20453b997a57434bac
Alex Essel <thefirmt@gmail.com>
2019-08-26 14:59:31 -0700

    Completed styling for individual entries onto the 'nearby' image carousel

--- client/components/CarouselEntry.jsx
+++ client/components/CarouselEntry.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 var carouselEntry = (props) => {
   return(
     <div id="full-item">
-      <div id="caro-img">{props.info.url}</div>
+      <div id="caro-img-box">
+        <img src={props.info.url} id="caro-img" />
+      </div>
       <div id="caro-cat">{props.info.cat}</div>
       <div id="caro-name">{props.info.desc}</div>
-      <div id="caro-price">{props.info.price}</div>
-      <div id="caro-star">{props.info.star}</div>
+      <div id="caro-price">From ${props.info.price}/person</div>
+      <div id="caro-star">{props.info.star} ★</div>
     </div>
   )
 }
 
 export default carouselEntry;
+
+//193.59 258.11
+//205 274
+//246 328
