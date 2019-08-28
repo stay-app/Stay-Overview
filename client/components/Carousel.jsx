e6850642fc0064d965a5386af7181e602a4ad83b
Alex Essel <thefirmt@gmail.com>
2019-08-28 16:53:57 -0700

    Completed sliding functionality to carousel component.

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -6,52 +6,57 @@ class Carousel extends React.Component {
     super(props);
 
     this.state = {
-      showing: []
+      showing: [],
+      idNext: '#5',
+      idPrev: '#1'
     }
 
     this.allNearby = this.props.info;
-    this.displayLast = 6;
-    this.displayFirst = 0;
+    this.displayLast = 5;
+    this.displayFirst = 1;
     this.moveDown = this.moveDown.bind(this);
     this.moveUp = this.moveUp.bind(this);
     }
 
     handleInfo() {
       if (this.props.info.length > 0) {
-      //   var liveDisplay = this.props.info.filter(ele => (ele.nearId < this.displayLast) && (ele.nearId > this.displayFirst))
         this.setState({
-          showing: this.props.info//liveDisplay
+          showing: this.props.info
         })
       }
     }
 
     moveUp() {
-      if (this.displayLast < 21) {
+      if (this.displayLast < 20) {
         this.displayLast++;
         this.displayFirst++;
         this.checkArrow();
       }
-      // this.handleInfo();
+      this.setState({
+        idNext: `#${this.displayLast}`
+      })
     }
 
     moveDown() {
-      if (this.displayFirst > 0) {
+      if (this.displayFirst > 1) {
         this.displayLast--;
         this.displayFirst--;
         this.checkArrow();
       }
-      // this.handleInfo();
+      this.setState({
+        idPrev: `#${this.displayFirst}`
+      })
     }
 
     checkArrow() {
       var prevArrow = document.getElementById('caro-prev')
       var nextArrow = document.getElementById('caro-next')
-      if (this.displayFirst === 0) {
+      if (this.displayFirst === 1) {
         prevArrow.className = 'caro-prev-white'
       } else {
         prevArrow.className = 'caro-prev'
       }
-      if (this.displayLast === 21) {
+      if (this.displayLast === 20) {
         nextArrow.className = 'caro-next-white'
       } else {
         nextArrow.className = 'caro-next'
@@ -64,13 +69,13 @@ class Carousel extends React.Component {
     }
       return(
         <div id="arrow-box">
-            <a className="caro-prev-white" id="caro-prev" onClick={this.moveDown}>&#60;</a>
+            <a className="caro-prev-white" id="caro-prev" onClick={this.moveDown} href={this.state.idPrev} >&#60;</a>
           <div id="carousel-box">
             {this.state.showing.map((ele, i) =>
               <CarouselEntry info={ele} key={i} />
               )}
           </div>
-            <a className="caro-next" id="caro-next" onClick={this.moveUp}>&#62;</a>
+            <a className="caro-next" id="caro-next" onClick={this.moveUp} href={this.state.idNext} >&#62;</a>
         </div>
     )
   }
