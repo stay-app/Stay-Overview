0c49cbd44a55ab00b9ee8d2af6ea3bb6d0dc3ae8
root <root@ip-172-31-13-163.us-west-1.compute.internal>
2019-09-07 19:38:27 +0000

    Adjustments for Docker pt7

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './client/index.jsx',
+  entry: './client/components/App.jsx',
 
   output: {
     library: 'Nearby',
